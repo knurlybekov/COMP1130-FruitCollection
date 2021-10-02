c95cb1ec6fffaf7895678e4ccd0d502ea797c4ed
Ompadhyay9 <90451128+Ompadhyay9@users.noreply.github.com>
2021-10-02 09:33:35 +0530

    Add files via upload

---
+++ Sapodilla.java
@@ -0,0 +1,8 @@
+package om;
+
+public class Sapodilla {
+  public String toString() {
+  return "This is a Sapodilla created by Om";
+}
+}
+
---
+++ SapodillaDriver.java
@@ -0,0 +1,11 @@
+package om;
+
+public class SapodillaDriver {
+
+	public static void main(String[] args) {
+	Sapodilla fruit = new Sapodilla();
+    System.out.println(fruit);
+
+	}
+
+}
