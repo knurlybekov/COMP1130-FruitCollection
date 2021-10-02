1355552a9657924a49cf94ecb161b2acbc65ab7a
Vinit Jangir <90442953+vinitjangir18@users.noreply.github.com>
2021-10-02 09:02:48 +0530

    Cherry Fruit
    I have completed this assignment using Cherry.java and CherryDriver.java classes with the toString();

---
+++ Cherry.java
@@ -0,0 +1,8 @@
+package Assignment_toString;
+
+public class Cherry {
+    public String toString() {
+        String toString = "This is Cherry created by Vinit";
+        return  toString;
+    }
+}
---
+++ CherryDriver.java
@@ -0,0 +1,8 @@
+package Assignment_toString;
+
+public class CherryDriver {
+    public static void main(String[] args){
+        Cherry fruit = new Cherry();
+        System.out.println(fruit);
+    }
+}
