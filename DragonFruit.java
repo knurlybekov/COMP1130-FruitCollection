d7d17d6e964cb89640ef4b4f4ffc758c615433e9
soodujjwal <90448948+soodujjwal@users.noreply.github.com>
2021-10-02 08:52:06 +0530

    DragonFruit created by Ujjwal Sood
    Sir i have completed the project and my fruit name is DragonFruit.

---
+++ DragonFruit.java
@@ -0,0 +1,8 @@
+package dragonfruit;
+
+public class DragonFruit {
+    public String toString(){
+       String toString = "This is a DragonFruit created by Ujjwal Sood";
+       return toString;
+    }
+}
