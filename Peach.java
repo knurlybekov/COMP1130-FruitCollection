e325932f277a72d343385b2bc34520675baadddd
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2021-10-05 04:16:41 +0600

    Add files via upload

---
+++ Peach.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Peach {
+    public String toString() {
+        return "This is an Peach created by Karen";
+    }
+}
---
+++ PeachDriver.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class PeachDriver {
+    public static void main(String[] args) {
+        Peach str = new Peach();
+        System.out.println(str);
+    }
+}
