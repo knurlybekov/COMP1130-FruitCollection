2665550983efd16f1a4bdc3b4a6f479daab00d36
Nikolay Sekerbayev <90434924+KolyaXGod@users.noreply.github.com>
2021-10-04 15:40:16 -0700

    Nikolay Sekerbayev
    Assignment Fruits, have a great day professor! :)

---
+++ TangerineDriver.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class TangerineDriver {
+
+        public static void main(String[] args) {
+            Tangerine str = new Tangerine();
+            System.out.println(str);
+        }
+    }
