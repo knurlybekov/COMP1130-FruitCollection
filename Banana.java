1dad7acf7194d0359f52c3d9140ae07b7fec0c6e
Behlah-0612 <90437004+Behlah-0612@users.noreply.github.com>
2021-10-05 11:30:05 +0530

    Assignment -Banana

---
+++ Banana.java
@@ -0,0 +1,6 @@
+public class Banana {
+	public String toString() {
+		return "This is a Banana created by Behlah";
+	}
+
+}
---
+++ BananaDriver.java
@@ -0,0 +1,14 @@
+
+public class BananaDriver
+{
+  public static void main(String[] args)
+  {
+    Banana Fruit = new Banana();
+    System.out.println(Fruit);
+    
+    Strawberry Sweet = new Strawberry();
+    System.out.println(Sweet);
+  }
+  
+    
+} 
