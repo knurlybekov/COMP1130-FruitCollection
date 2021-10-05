9238fd4145ebd67f32b030bf568bdeebedc07b02
Ompadhyay9 <90451128+Ompadhyay9@users.noreply.github.com>
2021-10-05 11:57:24 +0530

    Update SapodillaDriver.java

--- SapodillaDriver.java
+++ SapodillaDriver.java
@@ -4,8 +4,7 @@ public class SapodillaDriver {
 
 	public static void main(String[] args) {
 	Sapodilla fruit = new Sapodilla();
-    System.out.println(fruit);
-
-	}
+        System.out.println(fruit);
+}
 
 }
