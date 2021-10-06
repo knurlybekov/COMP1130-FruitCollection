084138374c5a0331ef23749d72eedd415668e822
Anjali Singh <90452967+AnjaliSingh2002@users.noreply.github.com>
2021-10-06 09:09:24 +0530

    Mango Fruit

---
+++ MangoDriver.java
@@ -0,0 +1,12 @@
+package Assignment_Fruit;
+
+public class MangoDriver {
+
+    public static void main(String[] args) {
+        Mango fruit = new Mango();
+        System.out.println(fruit);
+
+        }
+    }
+
+
