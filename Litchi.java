b96bc31af6027aa4aa7308ee532500bda057fe10
ZichenWang666 <90850073+ZichenWang666@users.noreply.github.com>
2021-10-06 11:28:35 +0800

    Add files via upload

---
+++ Litchi.java
@@ -0,0 +1,9 @@
+public class Litchi {
+
+    public String toString() {
+
+        return "This is a Litchi created by ZichenWang";
+
+    }
+
+}
---
+++ LitchiDriver.java
@@ -0,0 +1,10 @@
+public class LitchiDriver {
+
+    public static void main(String[] args) {
+
+        Litchi fruit = new Litchi();
+        System.out.println(fruit);
+
+    }
+
+}
