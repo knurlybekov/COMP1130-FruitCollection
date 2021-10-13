f27984f277e562b2c8db6b01d99c96492d4a6393
knurlybekov <75376694+knurlybekov@users.noreply.github.com>
2021-10-13 09:52:19 +0600

    Update Peach.java

--- Peach.java
+++ Peach.java
@@ -1,7 +1,13 @@
 package com.company;
-
 public class Peach {
+    private double weight;
     public String toString() {
         return "This is an Peach created by Karen";
+    }
+    public double getPeachWeight(){
+        return weight;
+    }
+    public void setPeachWeight(double PeachWeight) {
+        this.weight = PeachWeight;
     }
 }
